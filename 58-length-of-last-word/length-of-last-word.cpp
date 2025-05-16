c03af779e70199509e03b623290be49769a30a4c
ASHIQHUSSAIN-A <axshique@gmail.com>
2025-05-17 00:08:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.04%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+        int lengthOfLastWord(string s) {
+            int count=0;
+            int n =s.size()-1;
+            while(s[n]==' '){
+                n--;
+            }
+        for(int i=n;i>=0;i--){
+            if(s[i]==' '){
+            break;
+            }
+            else{
+                count++;
+            }
+        }
+        return count;    
+        }
+    };
