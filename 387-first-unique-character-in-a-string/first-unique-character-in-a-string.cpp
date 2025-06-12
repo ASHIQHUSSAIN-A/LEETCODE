1d1020bef2d4a83ca9481855e75ce3108e9e639e
ASHIQHUSSAIN-A <axshique@gmail.com>
2025-06-12 10:45:28 +0530

    Time: 15 ms (43.44%) | Memory: 14.9 MB (22.64%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+    unordered_map<char,int>m;
+    for(auto i:s)m[i]++;
+    for(int j=0;j<s.size();j++){
+        if(m[s[j]]==1){
+            return j;
+        }
+    }
+    return -1;    
+    }
+};
